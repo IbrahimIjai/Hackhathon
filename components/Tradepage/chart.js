44bcd3967b8b7715a80ec3fa94b6d89edb0dafe0
IbrahimIjai <64047050+IbrahimIjai@users.noreply.github.com>
2023-03-22 05:19:49 +0100

    semi finish

---
+++ components/Tradepage/chart.js
@@ -0,0 +1,69 @@
+// TradingViewWidget.jsx
+import Link from "next/link";
+
+import React, { useEffect, useRef } from "react";
+
+let tvScriptLoadingPromise;
+
+export default function TradingViewWidget() {
+  const onLoadScriptRef = useRef();
+
+  useEffect(() => {
+    onLoadScriptRef.current = createWidget;
+
+    if (!tvScriptLoadingPromise) {
+      tvScriptLoadingPromise = new Promise((resolve) => {
+        const script = document.createElement("script");
+        script.id = "tradingview-widget-loading-script";
+        script.src = "https://s3.tradingview.com/tv.js";
+        script.type = "text/javascript";
+        script.onload = resolve;
+
+        document.head.appendChild(script);
+      });
+    }
+
+    tvScriptLoadingPromise.then(
+      () => onLoadScriptRef.current && onLoadScriptRef.current(),
+    );
+
+    return () => (onLoadScriptRef.current = null);
+
+    function createWidget() {
+      if (
+        document.getElementById("tradingview_4ccd6") &&
+        "TradingView" in window
+      ) {
+        new window.TradingView.widget({
+          width: "100%",
+          height: "100%",
+          symbol: "BITSTAMP:BTCUSD",
+          interval: "30",
+          timezone: "Etc/UTC",
+          theme: "dark",
+          style: "1",
+          locale: "en",
+          toolbar_bg: "#f1f3f6",
+          enable_publishing: false,
+          allow_symbol_change: true,
+          container_id: "tradingview_4ccd6",
+        });
+      }
+    }
+  }, []);
+
+  return (
+    <div className="tradingview-widget-container">
+      <div id="tradingview_4ccd6" />
+      <div className="tradingview-widget-copyright">
+        <Link
+          href="https://www.tradingview.com/symbols/BTCUSD/?exchange=BITSTAMP"
+          rel="noopener"
+          target="_blank">
+          <span className="blue-text">Bitcoin chart</span>
+        </Link>{" "}
+        by TradingView
+      </div>
+    </div>
+  );
+}
--- pages/trades/[id].tsx
+++ pages/trades/[id].tsx
@@ -1,9 +1,11 @@
-import Chart from "@/components/Tradepage/chart";
+import TradingViewWidget from "@/components/Tradepage/chart";
 import Orderbook from "@/components/Tradepage/orderbook";
+
 export default function Home() {
   return (
     <>
       <Orderbook />
+        <TradingViewWidget />
     </>
   );
 }
